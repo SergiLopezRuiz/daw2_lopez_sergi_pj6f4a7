bdf29c052e4a617f822c09f0a86d79062e710bda
SergiLopezRuiz <459902.clot@fje.edu>
2025-10-16 15:15:45 +0200

    main.js

--- app/cli/js/main.js
+++ app/cli/js/main.js
@@ -1,16 +1,12 @@
-// explicación: manejadores de los botones de navegación
 window.addEventListener('DOMContentLoaded', () => {
   const goAbout = document.getElementById('goAbout');
   const goMenu  = document.getElementById('goMenu');
 
-  // explicación: redirige a las páginas que implementaremos en el siguiente paso
   goAbout?.addEventListener('click', () => {
-    // explicación: about.html contendrá la descripción/documentación
     window.location.href = 'about.html';
   });
 
   goMenu?.addEventListener('click', () => {
-    // explicación: menu.html contendrá la selección de operaciones (func. 3)
     window.location.href = 'menu.html';
   });
 });
